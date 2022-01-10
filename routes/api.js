35aaf4a8fe2f260d8969b78bfa034f1d94b59e7a
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2022-01-09 22:04:53 -0600

    added html routes and corrected api routes

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,70 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Workout = require("../models/Workout");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+//api route to get workouts
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration:
+          { $sum: '$exercises.duration' },
+      }
+    }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+//api route to get workout range
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration:
+          { $sum: '$exercises.duration' },
+        totalWeight:
+          { $sum: '$exercises.weight' }
+      }
+    }
+  ])
+    .sort({day:-1})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout.reverse());
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+//api route to create new workout
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
+});
+
+//api route to update workouts using the id
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
